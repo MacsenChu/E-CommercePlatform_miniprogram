eb76056b8d6a7ba9a4103acc57ef577b7997d67f
Macsen <7434311@qq.com>
2020-02-12 18:42:59 +0800

    完成了首页的开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,54 @@
-//Page Object
+// 0 引入用来发送请求的方法 一定要把路径补全
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/regenerator-runtime/runtime';
 Page({
   data: {
-    
+    // 轮播图数组
+    swiperList: [],
+    // 导航数组
+    catesList: [],
+    // 楼层数组
+    floorList: []
   },
-  //options(Object)
+  // 页面开始加载 就会触发
   onLoad: function(options) {
-    
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
   },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
 
+  // 获取轮播图数据
+  async getSwiperList () {
+    // 1 发送异步请求获取轮播图数据
+    // var reqTask = wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     console.log(result);
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+    let { data: res } =  await request({url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata'});
+    this.setData({
+      swiperList: res.message
+    });
   },
-  onUnload: function() {
 
+  // 获取导航数据
+  async getCateList() {
+    let { data: res } = await request({url: 'https://api.zbztb.cn/api/public/v1/home/catitems'});
+    this.setData({
+      catesList: res.message
+    });
   },
-  onPullDownRefresh: function() {
 
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
+  // 获取导航数据
+  async getFloorList() {
+    let { data: res } = await request({url: 'https://api.zbztb.cn/api/public/v1/home/floordata'});
+    this.setData({
+      floorList: res.message
+    });
   }
 });
   
